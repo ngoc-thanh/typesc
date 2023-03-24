05327bbfa9ec5eee5c66a1d31cb3433b33539f0e
ngoc-thanh <117883913+ngoc-thanh@users.noreply.github.com>
2023-03-24 20:28:48 +0700

    first commit

---
+++ Lesson1/dist/main.js
@@ -0,0 +1,8 @@
+var ts = "Xin chao ae";
+// console.log(ts);
+var ts1 = 11;
+console.log(ts1);
+var a = 10;
+if (typeof ts1 == "number") {
+    console.log(ts1 - a);
+}
---
+++ Lesson1/main.ts
@@ -0,0 +1,11 @@
+const ts: string = "Xin chao ae";
+// console.log(ts);
+let ts1: number=11;
+console.log(ts1);
+const a=10
+if (typeof ts1 == "number") {
+    console.log(ts1-a);
+}
+
+
+
